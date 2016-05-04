cd8d0d93de565cba0aedd3b05c9783b096e011ae
tyfoan <myonlygamess@gmail.com>
2016-05-05 01:55:54 +0300

    Differences between two files in bin/debug/result.txt

--- CallStatusService/CallStatusService/Program.cs
+++ CallStatusService/CallStatusService/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CallStatusService
+﻿namespace CallStatusService
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Service a = new Service(@"C:\Users\Александр\Documents\visual studio 2015\Projects\CallService\CallService\history task\1_04_2016\14.txt", 
+                @"C:\Users\Александр\Documents\visual studio 2015\Projects\CallService\CallService\history task\2_04_2016\14.txt");
+            a.Method();
         }
     }
 }
---
+++ CallStatusService/CallStatusService/Service.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallStatusService
+{
+    class Service
+    {
+        private string _previousFilePath;
+        private string _presentFilePath;
+        public Service(string previousFilePath, string presentFilePath)
+        {
+            _presentFilePath = presentFilePath;
+            _previousFilePath = previousFilePath;
+        }
+
+        public void Method()
+        {
+            //try
+            //{
+            //    using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            //    {
+            //        string line;
+            //        while ((line = sr.ReadLine()) != null)
+            //        {
+            //            Console.WriteLine(line);
+            //        }
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            string fileA, fileB, fileC;
+            fileA = _previousFilePath;
+            fileB = _presentFilePath;
+            fileC = "result.txt";
+
+            string alphaFilePath = fileA;
+
+            List<string> alphaFileContent = new List<string>();
+
+            using (FileStream fs = new FileStream(alphaFilePath, FileMode.Open))
+            using (StreamReader rdr = new StreamReader(fs))
+            {
+                while (!rdr.EndOfStream)
+                {
+                    alphaFileContent.Add(rdr.ReadLine());
+                }
+            }
+
+            string betaFilePath = fileB;
+
+            StringBuilder sb = new StringBuilder();
+
+
+            using (FileStream fs = new FileStream(betaFilePath, FileMode.Open))
+            using (StreamReader rdr = new StreamReader(fs))
+            {
+                while (!rdr.EndOfStream)
+                {
+                    string[] betaFileLine = rdr.ReadLine().Split(Convert.ToChar(","));
+
+                    if (!alphaFileContent.Contains(betaFileLine[0]))
+                    {
+                        sb.AppendLine(String.Format("{0}", betaFileLine[0]));
+                    }
+                }
+            }
+
+            using (FileStream fs = new FileStream(fileC, FileMode.Create))
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.Write(sb.ToString());
+            }
+        }
+
+    }
+}
